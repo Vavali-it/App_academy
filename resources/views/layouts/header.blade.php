eb8407396c251a515c8092f16224823584cb09d8
Liliya <liljsik1@gmail.com>
2020-02-19 14:51:40 +0200

    laravel

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,47 @@
+<body>
+<div class="container-fluid p-0">
+    <header>
+        <div class="d-sm-flex d-block justify-content-sm-between justify-content-end mr-3 mr-sm-5 text-right">
+            <div class="site-name d-none d-sm-block">Назва сайту</div>
+            <div class="text-right">
+                <a href="#">Реєстрація |</a> <a href="#">Вхід</a><br/>
+                <select class="leng">
+                    <option value="Українська">Українська</option>
+                    <option value="Англійська">Англійська</option>
+                </select>
+            </div>
+        </div>
+        <div class="d-flex d-sm-none justify-content-start">
+            <div class="hamburger-menu">
+                <input id="menu__toggle" type="checkbox"/>
+                <label class="menu__btn" for="menu__toggle">
+                    <span></span>
+                </label>
+                <ul class="menu__box text-left">
+                    <li><a class="menu__item" href="#">Головна</a></li>
+                    <li><a class="menu__item" href="#">Про нас</a></li>
+                    <li><a class="menu__item" href="#">Головний редактор</a></li>
+                    <li><a class="menu__item" href="#">Для авторів</a></li>
+                    <li><a class="menu__item" href="#">Поточний випуск</a></li>
+                    <li><a class="menu__item" href="#">Архів</a></li>
+                    <li><a class="menu__item" href="search.html">Пошук</a></li>
+                </ul>
+            </div>
+            <div class="site-name-mobile">Назва сайту</div>
+        </div>
+        <div class="d-none d-sm-block">
+            <nav class="navigation">
+                <ul class="nav">
+                    <li class="nav-item"><a class="nav-link active" href="#">Головна</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Про нас</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Головний редактор</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Для авторів</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Для рецензентів</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Поточний випуск</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Архів</a></li>
+                    <li class="nav-item"><a class="nav-link" href="search.html">Пошук</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,32 @@
+@extends ('layout')
+
+@section('content')
+<div id="presentation">
+    <div class="transparent float-right d-flex justify-content-around align-content-stretch">
+        <div class="d-flex flex-column justify-content-center">
+            <span class="name">Національний університет</span>
+            <span class="name">ОСТРОЗЬКА АКАДЕМІЯ</span>
+        </div>
+        <div class="d-flex flex-column justify-content-center">
+            <img class="logo float-right" src="/img/logo_1149.png" width="109" height="164" alt="Logo"/>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="card mb-3 mt-3 megaimg" style="max-width: 80%; margin: auto">
+        <div class="row  card-name">
+            <div class="col-md-4">
+                <img src="/img/facebook_375.svg" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Назва останнього випуску</h5>
+                    <p class="card-text">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                    <p class="card-text">Дата випуску: 2020 рік 15 січня</p>
+                    <p class="card-text">Скачать PDF</p>
+                </div>
+            </div>
+        </div>
+    </div>
+            </div>
+<div class="clearfix"></div>
+@endsection
